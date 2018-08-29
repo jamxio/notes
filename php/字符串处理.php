0c3261fba0d0a7e599caa30d27db045da6ba87fb
jamxio <jamxio-w@hotmail.com>
2018-08-29 11:58:20 +0800

    新提交

---
+++ php/字符串处理.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * substr();和strlen();对utf的每个字符的长度都并不是1，
+ * 例如每个utf-8bm4的都是3
+ * 所以要用mb_substr();和mb_strlen();
+ * 可是str_pad没有mb_*，应该怎么办
+ */
+echo substr('abcdef',2,3).PHP_EOL;
+var_dump(substr('啊波次的额佛',2,3));
+var_dump(substr('啊波次的额佛',3,3));
+print(mb_substr('啊波次的额佛',2,3).PHP_EOL);
+print_r(str_pad('我',5,'-').PHP_EOL);
+print(str_pad('wo',5,'-'));
